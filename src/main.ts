a7c774c2bb6bf281a3d369d11990227c702b4789
Erick Christian <erickchristian48@gmail.com>
2025-05-13 18:42:42 +0700

    add design by gemini

--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,19 @@
-import { LitElement, css, html } from "lit";
-import { customElement, property } from "lit/decorators.js";
+import { LitElement, html } from "lit";
+import { customElement } from "lit/decorators.js";
 
 @customElement("main-app")
 export class MainApp extends LitElement {
-  @property()
-  docsHint = "Click on the Vite and Lit logos to learn more";
-
-  @property({ type: Number })
-  count = 0;
-
   render() {
     return html`
-      <div class="card">
-        <button @click=${this._onClick} part="button">
-          count is ${this.count}
-        </button>
+      <div>
+        Turn anything into a pokemon
+
+        <textarea
+          placeholder="Short description of the pokemon"
+          id="input"
+        ></textarea>
+        <button>Generate</button>
       </div>
-      <p class="read-the-docs">${this.docsHint}</p>
     `;
   }
-
-  private _onClick() {
-    this.count++;
-  }
-
-  static styles = css`
-    :host {
-      max-width: 1280px;
-      margin: 0 auto;
-      padding: 2rem;
-      text-align: center;
-    }
-  `;
 }
